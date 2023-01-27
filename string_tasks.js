2c24288004283826b5ab1d666edc366a026e61fc
Sumaiya Junaid <90271730+SMYJND@users.noreply.github.com>
2023-01-27 12:13:48 +0530

    string tasks

---
+++ string_tasks.js
@@ -0,0 +1,45 @@
+console.log("1: Print out all characters of a string");
+
+// let s = "String";
+// for (let i = 0; i < s.length; i++) {
+//     console.log(s[i]);
+// }
+
+let str = 'string';
+console.log(str.split(''));
+console.log(str.trim());
+
+console.log(" 2: Print out all characters of a string in reversed order");
+
+// let inputstr = "String";
+// for (let i = inputstr.length-1; i>=0; i--) {
+//     console.log(inputstr[i]);
+// }
+
+console.log(str.split('').reverse());
+
+console.log("3: Write a program, that reverts a string");
+
+console.log(str.split('').reverse().join(''));
+
+console.log("4: Write a program, that detects palindromes");
+let term = 'racecar';
+let reterm = (word.split("").reverse().join(""));
+if(reterm==term){
+    console.log("Palindrome");
+}
+else{
+    console.log("Not a palindrome");
+}
+
+
+console.log("5: Write a program, that counts how often each character is included in a string")
+// console.log(str.split())
+
+
+console.log("6: Write a number, that prints out the first n characters of a string only")
+
+console.log("7: Write a program, that concatinates a given string n times")
+
+console.log("8: Write a program, that inserts a character into a string at a given position")
+
